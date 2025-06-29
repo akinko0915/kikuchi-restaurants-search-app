815a8a5a7f238cd0283c32db3b66fd914b7c1d78
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-29 21:55:07 +0900

    :tada: Add area and genre search

--- reference/script.js
+++ reference/script.js
@@ -1,28 +1,37 @@
-const keyword = document.getElementById("keyword");
+const area = document.getElementById("area");
+const genre = document.getElementById("genre");
 const button = document.getElementById("searchButton");
 const result = document.getElementById("results");
 
-url = "http://127.0.0.1:3000/restaurants";
-getRestaurants = () => {
-  return fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP error " + response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-    });
+url = "http://127.0.0.1:3000/api/v1/restaurants";
+getRestaurants = async (area, genre) => {
+  try {
+    const query = `?area=${encodeURIComponent(area)}&genre=${encodeURIComponent(
+      genre
+    )}`;
+    const response = await fetch(url + query);
+
+    if (!response.ok) throw new Error("HTTP error" + response.status);
+
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return [];
+  }
 };
+
 button.addEventListener("click", () => {
-  getRestaurants().then((restaurants) => {
-    if (!restaurants) return;
-    console.log(restaurants);
-    // let extracted_restaurants = restaurants.genre.filter("keyword");
-    // console.log(extracted_restaurants);
+  getRestaurants(area.value, genre.value).then((restaurants) => {
+    result.innerText = "";
+    restaurants.forEach((restaurant, index) => {
+      result.innerText += `レストラン ${index + 1}:\n`;
+      result.innerText += `名前: ${restaurant.name}\n`;
+      result.innerText += `住所: ${restaurant.address}\n`;
+      result.innerText += `エリア: ${restaurant.area.name}\n`;
+      result.innerText += `ジャンル: ${restaurant.genres
+        .map((g) => g.name)
+        .join("、")}\n`;
+      result.innerText += "------\n";
+    });
   });
 });
