77f22e895f17e459fd19f2741dfa767fd5fae59b
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-07-10 21:45:14 +0900

    remove description from docs

--- docs/script.js
+++ docs/script.js
@@ -7,14 +7,11 @@ const url = "https://kikuchi-restaurants-api.onrender.com/api/v1/restaurants";
 
 const getRestaurants = async (params) => {
   try {
-    // console.log(params)
     const query = new URLSearchParams(params).toString();
     const response = await fetch(`${url}?${query}`);
 
-    // console.log(response);
     if (!response.ok) throw new Error("HTTP error " + response.status);
 
-    // console.log(response.json())
     return await response.json();
   } catch (error) {
     console.error("Fetch error:", error);
@@ -22,24 +19,18 @@ const getRestaurants = async (params) => {
   }
 };
 
-// event handler?
 button.addEventListener("click", () => {
-  // console.logで出す
   const area = areaSelect.value;
   const genre = genreSelect.value;
 
-  // 簡単な処理で例を見せてから
   const searchParams = {
     area: area,
     genre: genre,
   };
 
   getRestaurants(searchParams).then((restaurants) => {
-    // 初期化する
     result.innerHTML = "";
 
-    // console.log(restaurants);
-    /** 取得したデータがなかったときの処理**/
     if (restaurants.length === 0) {
       const messageDiv = document.createElement("div");
       messageDiv.className = "no-results";
@@ -48,9 +39,6 @@ button.addEventListener("click", () => {
       return;
     }
 
-    // 取得したデータがあるときの処理 (説明しながら)
-    // まず画面見せてから（一つのデータの場合→複数のデータの場合）
-    // ステップを書いておく
     restaurants.forEach((restaurant) => {
       const shopDiv = document.createElement("div");
       shopDiv.className = "shop";
